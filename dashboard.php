39fadbe567d61107e98de9ceb3075dbe1b6c6bab
ItsJennyMadria <ihopeyougetbanzaid@gmail.com>
2026-05-03 15:38:21 +0800

    Finalized dashboard layout and responsive product images

--- dashboard.php
+++ dashboard.php
@@ -23,7 +23,7 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : "Guest";
 ?>
 <nav class="nike-navbar">
     <div class="logo">
-        <img src="assets/images/nike-swoosh.png" alt="Nike Logo" width="60">
+        <img src="assets/images/Nike black logo.jfif" alt="Nike Logo" width="60">
     </div>
     <ul class="nav-links">
         <li><a href="#">New Releases</a></li>
@@ -39,7 +39,7 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : "Guest";
     <h2>New Nike Releases</h2>
     <div class="product-grid">
         <div class="product-card">
-            <img src="assets/images/air-force-1.jpg" alt="Air Force 1">
+            <img src="assets/images/NikeAir Force 1 '07.jpg" alt="Air Force 1">
             <h3>Air Force 1 '07</h3>
             <p>₱5,495</p>
             <button class="buy-btn">Add to Bag</button>
